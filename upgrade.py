cdd845b992364cc909f4c3d5e3748aff7e6cd28a
esapgan <gangwar.sapna@gmail.com>
2021-06-10 15:07:13 -0700

    switch upgrade script without prompt

--- upgrade.py
+++ upgrade.py
@@ -2,42 +2,63 @@
 
 import netmiko
 import time
+import re
+from datetime import datetime
 
 #connect to a switch
-connection= netmiko.ConnectHandler(ip='10.81.117.55',device_type='arista_eos',username='admin',password='')
 
-print(connection.send_command('show ip int brief'))
-dir_op=connection.send_command('dir')
-print(dir_op)
-image=input('Input the image in 4.x.x format: ')
-#print(image)
-eos_image='EOS-'+image+'.swi'
-print(eos_image)
+class SwitchUpgrade():
 
-#check if the imgae is already present in the flash
-if eos_image in dir_op:
-    config_commands=['config','boot system flash:'+eos_image]
-    #print(config_commands)
-    output=connection.send_config_set(config_commands)
-    print(output)
+    def connection():
 
-else:
-    config_commands=['config','getimage '+image]
-    print(config_commands)
-    connection.send_config_set(config_commands)
-    print('Downloading '+eos_image)
-    time.sleep(120)
-    config_commands=['config','boot system flash:'+eos_image]
-    connection.send_config_set(config_commands)
+        mgmt_ip=input('Enter the mgmt IP of the switch: ')
+        user=input('Enter the switch username: ')
+        pswrd=input('Enter the switch password: ')
+        connection= netmiko.ConnectHandler(ip=mgmt_ip,device_type='arista_eos',username=user,password=pswrd,fast_cli=False)
+        print('connection successful')
+        return connection
 
-#check status and reload
-output=connection.send_command('show boot-config')
-print(output)
-confirm=input('Is boot-config looks good, do you want to reload? Enter Yes/No: ')
-if confirm=='Yes':
-    connection.send_command('reload now')
-else:
-    print('You dont want to reboot, that is fine, connection will be closed')
+    def outputs(connection):
+        try: 
+            print(connection.send_command('show version'))
+            dir_op=connection.send_command('dir')
+            image=input('Input the image in 4.x.xF/M format: ')
+            #print(image)
+            eos_image='EOS-'+image+'.swi'
+            print('Upgrading to '+eos_image)
 
-#close the connection
-connection.disconnect()
+            #check if the imgae is already present in the flash
+            if eos_image in dir_op:
+                connection.send_config_set('boot system flash:'+eos_image)
+
+            else:
+                start_time=datetime.now()
+                cmnd=('getimage '+image)
+                #output=connection.send_command(cmnd)
+                #time.sleep(180)
+                output =connection.send_command(cmnd,delay_factor=5)
+                config_commands=['boot system flash:'+eos_image]
+                connection.send_config_set(config_commands)
+
+            #check status and reload
+            output=connection.send_command('show boot-config')
+            print(output)
+            confirm=input('Is boot-config looks good, do you want to reload? Enter Yes/No: ')
+            if confirm=='Yes':
+                connection.send_command('reload now')
+            else:
+                print('You dont want to reboot, that is fine, connection will be closed')
+                connection.disconnect()
+        except:
+            print('except')
+            connection.disconnect()
+
+def main():
+
+    connect=SwitchUpgrade.connection()
+    output=SwitchUpgrade.outputs(connect)
+
+if __name__=='__main__':
+    main()
+
+
